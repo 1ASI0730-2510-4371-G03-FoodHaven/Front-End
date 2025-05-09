5a4cf86ca4e2fa5fe7d65217f836fd0ffc14a324
Paulo02-pixel <u20191e562@upc.edu.pe>
2025-05-08 22:37:28 -0500

    Configuración de base para despliegue en GitHub Pages
    # Conflicts:
    #	vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'https://1ASI0730-2510-4371-G03-FoodHaven.github.io/Front-End/',
   plugins: [
     vue(),
     vueDevTools(),
